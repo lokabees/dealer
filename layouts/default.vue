9793e8ab4c6e260a8fc1807392652bdf58f0f8c6
Lukas Mauser <l.mauser.dev@gmail.com>
2020-07-22 11:53:48 +0200

    Fix: temporarily disable v-lazy at footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,9 +4,9 @@
     <modal :show="showModal" centered></modal>
     <navigation class="hidden md:block sticky top-0 z-30" />
     <nuxt class="container flex-1 mx-auto md:mb-10" />
-    <v-lazy ssr-only>
-      <foot />
-    </v-lazy>
+    <!--  <v-lazy ssr-only> -->
+    <foot />
+    <!--  </v-lazy> -->
     <v-lazy ssr-only>
       <navbar class="block md:hidden fixed bottom-0 w-full" />
     </v-lazy>
