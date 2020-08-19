ec7d3ca9a75e88acbac709d27ae3ba0ca3f5695f
Lukas Mauser <l.mauser.dev@gmail.com>
2020-08-19 12:12:49 +0200

    Fix: reset password validation

--- pages/auth/reset-password.vue
+++ pages/auth/reset-password.vue
@@ -2,11 +2,14 @@
   <div>
     <h1>{{ $t('reset_password.title') }}</h1>
     <p>{{ $t('reset_password.text') }}</p>
-    <FormulateForm>
+    <FormulateForm @submit="resetPassword">
       <FormulateInput
+        v-model="email"
+        name="Email"
         type="email"
         :label="$t('reset_password.email')"
         :placeholder="$t('reset_password.email_placeholder')"
+        validation="required|email"
       />
       <FormulateInput type="submit" :label="$t('reset_password.submit')" />
     </FormulateForm>
@@ -15,5 +18,16 @@
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      email: '',
+    }
+  },
+  methods: {
+    resetPassword() {
+      console.log('reset password ' + this.email)
+    },
+  },
+}
 </script>
