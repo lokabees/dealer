0d63f50db7ffaef1e1f2490405846d8488cf6a5d
Lukas Mauser <l.mauser.dev@gmail.com>
2020-08-19 12:01:31 +0200

    Feat: reset password page

---
+++ pages/auth/reset-password.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>{{ $t('reset_password.title') }}</h1>
+    <p>{{ $t('reset_password.text') }}</p>
+    <FormulateForm>
+      <FormulateInput
+        type="email"
+        :label="$t('reset_password.email')"
+        :placeholder="$t('reset_password.email_placeholder')"
+      />
+      <FormulateInput type="submit" :label="$t('reset_password.submit')" />
+    </FormulateForm>
+    <button>{{ $t('reset_password.back') }}</button>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
