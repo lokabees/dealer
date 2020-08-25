654634bee1e3081eb092bbbb03acb4fea96beb75
Lukas Mauser <l.mauser.dev@gmail.com>
2020-08-25 12:17:42 +0200

    Fix: logout

--- components/layout/Navigation.vue
+++ components/layout/Navigation.vue
@@ -2,15 +2,20 @@
   <div class="container prose">
     <n-link to="/"><img src="/img/logo.svg" width="150px" /></n-link>
     <button @click="$router.push('/user')">Profile</button>
-    <button @click="logoutUser">Sign out</button>
+    <button v-if="$store.state.accessToken" @click="logoutUser">
+      Sign out
+    </button>
   </div>
 </template>
 
 <script>
-import { mapActions } from 'vuex'
+import { mapActions, mapMutations } from 'vuex'
 export default {
   methods: {
     ...mapActions(['resetUser']),
+    ...mapMutations('modal', {
+      showModal: 'showModal',
+    }),
     async logoutUser() {
       try {
         await this.$axios.post(`/api/auth/logout`)
@@ -19,7 +24,10 @@ export default {
         // Send toast
         // this.$store.dispatch('toast/add', { message: `toast.logout` })
       } catch (error) {
+        this.resetUser()
+        this.$router.push('/auth/login')
         console.error(error)
+        this.showModal(error)
       }
     },
   },
