c85c6a3b5793bfe4f76399aa5e1d01ce75ffe4db
Jonas Scholz <jonas.scholz@bbscholz.de>
2020-09-14 14:19:18 +0200

    Feat: Added logout-success page

--- components/layout/Navigation.vue
+++ components/layout/Navigation.vue
@@ -25,7 +25,7 @@ export default {
       try {
         await this.$axios.post(`/api/auth/logout`)
         this.resetUser()
-        this.$router.push('/auth/login')
+        this.$router.push('/auth/logout-success')
         // Send toast
         // this.$store.dispatch('toast/add', { message: `toast.logout` })
       } catch (error) {
---
+++ pages/auth/logout-success.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container max-w-md p-3 md:p-0 flex">
+    <div class="m-auto">
+      <div class="flex justify-center text-primary my-8">
+        <icon name="checkmark-circle-outline" height="100" width="100" />
+      </div>
+      <div class="prose lg:prose-lg text-center">
+        <h2 class="leading-tight">
+          {{ $t('logout.successful') }}
+        </h2>
+        <p>{{ $t('logout.sub') }}</p>
+      </div>
+      <button class="primary w-full mt-8" @click="$router.push('/auth/login')">
+        {{ $t('signup.to_login') }}
+      </button>
+    </div>
+  </div>
+</template>
