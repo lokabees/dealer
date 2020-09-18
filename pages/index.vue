dd31ad792094cccf1d40e6a90ef043b675915dd0
Lukas Mauser <l.mauser.dev@gmail.com>
2020-09-18 11:19:18 +0200

    Style: dashboard

--- pages/index.vue
+++ pages/index.vue
@@ -1,48 +1,60 @@
 <template>
-  <div class="container prose">
-    <h1>{{ shopName }}</h1>
+  <div class="md:container mx-auto prose">
+    <img :src="coverImage" class="h-48 w-full object-cover" />
 
-    store img outside
-    <img :src="coverImage" width="150px" />
-    store img inside
-    <img :src="profileImage" width="150px" />
-
-    <button @click="$router.push('/shop/create')">
-      create store (delete this button)
-    </button>
-
-    <button class="primary" @click="$router.push('/shop/edit')">
-      {{ $t('dashboard.store.manage_store') }}
-    </button>
-    <button class="tertiary" @click="$router.push('/')">
-      {{ $t('dashboard.store.view_store') }}
-    </button>
-
-    <div>
-      <img src="icon.png" width="150px" />
-      <h2>{{ $t('dashboard.store.title') }}</h2>
-      <p>{{ $t('dashboard.store.text') }}</p>
-      <button class="secondary" @click="$router.push('/shop/edit')">
-        {{ $t('dashboard.store.manage') }}
-      </button>
+    <div class="flex flex-wrap overflow-hidden">
+      <div>
+        <img :src="profileImage" class="h-48 w-48 object-cover" />
+      </div>
+      <div class="mx-auto">
+        <button class="primary" @click="$router.push('/shop/edit')">
+          {{ $t('dashboard.store.manage_store') }}
+        </button>
+        <button class="tertiary" @click="$router.push('/')">
+          {{ $t('dashboard.store.view_store') }}
+        </button>
+      </div>
     </div>
-    <div>
-      <img src="icon.png" width="150px" />
-      <h2>{{ $t('dashboard.products.title') }}</h2>
-      <p>{{ $t('dashboard.products.text') }}</p>
-      <button class="secondary" @click="$router.push('/products')">
-        {{ $t('dashboard.products.manage') }}
-      </button>
+
+    <div class="flex">
+      <div>
+        <h1>{{ shopName }}</h1>
+      </div>
+      <div class="flex justify-end bg-gray-200">
+        Categories
+      </div>
     </div>
-    <div>
-      <img src="icon.png" width="150px" />
-      <h2>{{ $t('dashboard.academy.title') }}</h2>
-      <p>{{ $t('dashboard.academy.text') }}</p>
-    </div>
-    <div>
-      <img src="icon.png" width="150px" />
-      <h2>{{ $t('dashboard.analysis.title') }}</h2>
-      <p>{{ $t('dashboard.analysis.text') }}</p>
+
+    <div class="flex flex-wrap overflow-hidden -mx-2">
+      <div class="dashboard-card">
+        <img src="icon.png" />
+        <h2>{{ $t('dashboard.store.title') }}</h2>
+        <p>{{ $t('dashboard.store.text') }}</p>
+        <button class="secondary" @click="$router.push('/shop/edit')">
+          {{ $t('dashboard.store.manage') }}
+        </button>
+      </div>
+
+      <div class="dashboard-card">
+        <img src="icon.png" />
+        <h2>{{ $t('dashboard.products.title') }}</h2>
+        <p>{{ $t('dashboard.products.text') }}</p>
+        <button class="secondary" @click="$router.push('/products')">
+          {{ $t('dashboard.products.manage') }}
+        </button>
+      </div>
+
+      <div class="dashboard-card">
+        <img src="icon.png" />
+        <h2>{{ $t('dashboard.academy.title') }}</h2>
+        <p>{{ $t('dashboard.academy.text') }}</p>
+      </div>
+
+      <div class="dashboard-card">
+        <img src="icon.png" />
+        <h2>{{ $t('dashboard.analysis.title') }}</h2>
+        <p>{{ $t('dashboard.analysis.text') }}</p>
+      </div>
     </div>
   </div>
 </template>
@@ -56,10 +68,12 @@ export default {
       shop: 'activeShop',
     }),
     coverImage() {
-      return this.shop?.images?.cover?.url
+      // return this.shop?.images?.cover?.url || 'icon.png'
+      return 'icon.png'
     },
     profileImage() {
-      return this.shop?.images?.profile?.url
+      // return this.shop?.images?.profile?.url || 'icon.png'
+      return 'icon.png'
     },
     shopName() {
       return this.shop?.name
@@ -67,3 +81,17 @@ export default {
   },
 }
 </script>
+
+<style lang="scss">
+.dashboard-card {
+  @apply w-full overflow-hidden px-2 my-2;
+
+  @screen sm {
+    @apply w-1/2;
+  }
+
+  img {
+    @apply w-full h-52 object-cover;
+  }
+}
+</style>
