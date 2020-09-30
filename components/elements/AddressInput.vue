d8a65561f9e54e7ccbce20c280a3dfcffdca8ef9
Lukas Mauser <l.mauser.dev@gmail.com>
2020-09-30 13:31:50 +0200

    Style: address input

--- components/elements/AddressInput.vue
+++ components/elements/AddressInput.vue
@@ -1,40 +1,58 @@
 <template>
-  <FormulateInput type="group" name="address" class="flex">
+  <FormulateInput type="group" name="address">
     <!-- TODO custom validation: street and name can have spaces, but no numbers-->
-    <FormulateInput
-      v-model="context.model.street"
-      type="text"
-      name="street"
-      :placeholder="$t('shop_registration_wizard.step_1.street_placeholder')"
-      :label="$t('shop_registration_wizard.step_1.street')"
-      validation="required"
-    />
-    <FormulateInput
-      v-model="context.model.number"
-      type="text"
-      name="number"
-      :placeholder="$t('shop_registration_wizard.step_1.number_placeholder')"
-      :label="$t('shop_registration_wizard.step_1.number')"
-      validation="required|number"
-    />
-    <FormulateInput
-      v-model="context.model.postcode"
-      type="text"
-      name="postcode"
-      :placeholder="
-        $t('shop_registration_wizard.step_1.postal_code_placeholder')
-      "
-      :label="$t('shop_registration_wizard.step_1.postal_code')"
-      validation="required"
-    />
-    <FormulateInput
-      v-model="context.model.city"
-      type="text"
-      name="city"
-      :placeholder="$t('shop_registration_wizard.step_1.city_placeholder')"
-      :label="$t('shop_registration_wizard.step_1.city')"
-      validation="required"
-    />
+    <div class="flex">
+      <div class="w-full">
+        <FormulateInput
+          v-model="context.model.street"
+          outer-class="flex-initial"
+          type="text"
+          name="street"
+          :placeholder="
+            $t('shop_registration_wizard.step_1.street_placeholder')
+          "
+          :label="$t('shop_registration_wizard.step_1.street')"
+          validation="required"
+        />
+      </div>
+      <div>
+        <FormulateInput
+          v-model="context.model.number"
+          outer-class="flex-initial"
+          type="text"
+          name="number"
+          :placeholder="
+            $t('shop_registration_wizard.step_1.number_placeholder')
+          "
+          :label="$t('shop_registration_wizard.step_1.number')"
+          validation="required|number"
+        />
+      </div>
+    </div>
+    <div class="flex">
+      <div>
+        <FormulateInput
+          v-model="context.model.postcode"
+          type="text"
+          name="postcode"
+          :placeholder="
+            $t('shop_registration_wizard.step_1.postal_code_placeholder')
+          "
+          :label="$t('shop_registration_wizard.step_1.postal_code')"
+          validation="required"
+        />
+      </div>
+      <div class="w-full">
+        <FormulateInput
+          v-model="context.model.city"
+          type="text"
+          name="city"
+          :placeholder="$t('shop_registration_wizard.step_1.city_placeholder')"
+          :label="$t('shop_registration_wizard.step_1.city')"
+          validation="required"
+        />
+      </div>
+    </div>
   </FormulateInput>
 </template>
 
