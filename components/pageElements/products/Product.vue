3396400ab7099ef78715ec20589df07984073bde
Lukas <k>
2020-11-20 19:40:25 +0100

    Style: products

--- components/pageElements/products/Product.vue
+++ components/pageElements/products/Product.vue
@@ -4,7 +4,7 @@
       <div class="relative h-32 w-32 flex-none rounded-lg my-4">
         <slot name="img"></slot>
       </div>
-      <div class="pl-5">
+      <div class="md:pl-5 pb-2">
         <slot />
       </div>
     </div>
